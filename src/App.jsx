bb4a80211c5dffb8bb24ef4a990e004e371f17b1
Suraj Kumar <w4rsuraj99@gmail.com>
2025-12-10 16:23:21 +0530

    finailize app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,32 @@ import './App.css';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+      {/* Navigation bar - shows on all pages except 404 */}
+      <Routes>
+        <Route path="*" element={null} />
+        <Route path="/" element={<Navbar />} />
+        <Route path="/browse" element={<Navbar />} />
+        <Route path="/browse/:category" element={<Navbar />} />
+        <Route path="/book/:id" element={<Navbar />} />
+        <Route path="/add-book" element={<Navbar />} />
+      </Routes>
+
       <main className="container mx-auto px-4 py-8">
         <Routes>
+          {/* Home Page */}
           <Route path="/" element={<Home />} />
+
+          {/* Browse Books Page with category filtering */}
           <Route path="/browse" element={<BrowseBooks />} />
           <Route path="/browse/:category" element={<BrowseBooks />} />
+
+          {/* Book Details Page */}
           <Route path="/book/:id" element={<BookDetails />} />
+
+          {/* Add Book Page */}
           <Route path="/add-book" element={<AddBook />} />
+
+          {/* 404 Page - Catch all undefined routes */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
